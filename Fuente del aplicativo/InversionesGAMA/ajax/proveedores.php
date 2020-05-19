cc390506959978e725ea62c0b37cb30cce17a3c6
shadow145 <60850932+shadow145@users.noreply.github.com>
2020-05-19 17:41:48 -0600

    Update proveedores.php

--- Fuente del aplicativo/InversionesGAMA/ajax/proveedores.php
+++ Fuente del aplicativo/InversionesGAMA/ajax/proveedores.php
@@ -1,16 +1,15 @@
 <?php
-    $accion = $_GET["accion"];
-    switch ($accion) {
-        case 'registrar':
+
+        registrar
             // Insertar los siguientes datos SQL en tabla Proveedor
             /*$_GET["rtn"], $_GET["nombreProveedor"], $_GET["tipo"], $_GET["direccion"], $_GET["cantidad"], $_GET["precio"]*/
 
             $respuesta["codigo"] = 1;
             $respuesta["mensaje"] ='Proveedor registrado correctamente';
             echo json_encode($respuesta);
-            break;
+    
 
-        case 'abrirModal':
+        abrirModal
             // Buscar del elemento con el id = $_GET["rtn"] los siguientes datos SQL: $respuesta["nombreProveedor"], $_GET["rtn"], $_GET["tipo"], $_GET["direccion"], $_GEt["telefono"], $_GET["email"], $_GET["estado"]
                 
             $respuesta["nombreProveedor"] = "Juanin Juan Jarris";
@@ -21,19 +20,15 @@
             $respuesta["email"] = "example@assa.adas";
             $respuesta["estado"] = 1;
             echo json_encode($respuesta);
-            break;
             
-        case 'editar':
+        editar
             // Editar el elemento con el id = $_GET["rtn"] conn los siguientes datos: $_GET["direccion"], $_GET["telefono"], $_GET["email"], $_GET["estado"];
             
             //El SP debe SOLA Y UNICAMENTE devolver codigo y mensaje
             $respuesta["codigo"] = 1;
             $respuesta["mensaje"] ='Insumo editado correctamente';
             echo json_encode($respuesta);
-            break;
+          
             
-        case 'listaProveedores':
-            # code...
-            break;
     }
 ?>
