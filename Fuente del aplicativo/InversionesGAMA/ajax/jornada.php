13cf6db90903a686d5341eb0c0dac484509f25d4
shadow145 <60850932+shadow145@users.noreply.github.com>
2020-05-19 17:29:30 -0600

    Update jornada.php

--- Fuente del aplicativo/InversionesGAMA/ajax/jornada.php
+++ Fuente del aplicativo/InversionesGAMA/ajax/jornada.php
@@ -1,13 +1,12 @@
 <?php
-    $accion = $_GET["accion"];
-    switch ($accion) {
-        case 'registrar':
+    
+     registrar
             $respuesta["codigo"] = 1;
             $respuesta["mensaje"] ='Jornada registrada correctamente';
             echo json_encode($respuesta);
-            break;
+       
 
-        case 'abrirModal':
+      abrirModal
             // Buscar información del elemento con el id = $_GET["id"]
             $respuesta["mensaje"] = "Apertura ventana modal para editar insumo";
             
@@ -15,19 +14,17 @@
             $respuesta["precio"] = 25.8;
             $respuesta["nombre"] ='Nombre insumo';
             echo json_encode($respuesta);
-            break;
+          
             
-        case 'editar':
+        editar
             // Editar el elemento con el id = $_GET["id"]
             $respuesta["mensaje"] = "Se ha completado la edición de un insumo";
             
             $respuesta["cantidad"] = $_GET["cantidad"];
             $respuesta["precio"] = $_GET["precio"];
             echo json_encode($respuesta);
-            break;
+     
             
-        case 'listaJornadas':
-            # code...
-            break;
+      
     }
 ?>
